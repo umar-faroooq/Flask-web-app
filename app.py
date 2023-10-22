0587ca395a08a8fd2fa29e787792de63f6c1c0f5
Umar Farooq <umar5555f@gmail.com>
2023-10-23 01:00:18 +0500

    Added Dynamic data and api routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 
 app = Flask(__name__)
@@ -34,9 +34,12 @@ JOBS = [
 def home():
     return render_template('home.html', jobs=JOBS, company_name="Nexoleaf")
 
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
+
 
 if __name__=="__main__":
     app.run(debug=True)
 
 
-    
